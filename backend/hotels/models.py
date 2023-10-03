12e13b97db31e9eee432b4d785d5f0ece84b24f2
Mahedi-Hasan10 <hasanmdmahedi193@gmail.com>
2023-10-03 20:19:30 +0600

    first commit

--- backend/hotels/models.py
+++ backend/hotels/models.py
@@ -6,7 +6,7 @@ class Hotel(models.Model):
     hotel_name = models.CharField(max_length= 100, unique=True)
     description = models.TextField(max_length= 500, blank=True)
     price = models.IntegerField()
-    image = models.ImageField(upload_to = 'photos/products', null=True, blank=True)
+    image = models.ImageField(upload_to = 'hotels/', null=True, blank=True)
     location = models.CharField(max_length=100, default='YourDefaultLocation')
 
     def __str__(self):
